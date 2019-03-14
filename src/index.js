646eb55fcdc66f242effe8de8af75b290edb17e6
knguyen1 <knguyen1@paypal.com>
2019-03-13 17:34:06 -0700

    Add trackLastCommit.js

---
+++ src/index.js
@@ -0,0 +1,3 @@
+/*track-last-commit_1.0.0_39f2219_2019313*/
+// Insert sophisticated codebase here
+console.log("Hello World!");
---
+++ trackLastCommit.js
@@ -0,0 +1,21 @@
+const shell = require("shelljs");
+const childProcess = require("child_process");
+const path = require("path");
+
+const gitHashLength = 7;
+const gitHash = childProcess
+  .execSync("git rev-parse HEAD")
+  .toString()
+  .substring(0, gitHashLength);
+const buildDate = new Date();
+const month = buildDate.getMonth() + 1;
+const packageVersion = `${require("./package.json").version}`;
+const packageName = `${require("./package.json").name}`;
+
+shell.exec(
+  `echo "/*${packageName}_${packageVersion}_${gitHash}_${buildDate.getFullYear()}${month}${buildDate.getDate()}*/"|cat - ${path.resolve(
+    __dirname,
+    "src/index.js"
+  )} > /tmp/out
+  && mv /tmp/out ${path.resolve(__dirname, "src/index.js")}`
+);
